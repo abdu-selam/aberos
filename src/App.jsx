c5a07f66180c35de79e8265567ebf17724de152f
Abduselam Awel <abdselam676@gmail.com>
2026-01-31 04:06:11 -0400

    Routing issue has been resolved

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from "react-router";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router";
 import ScrollToTop from "./components/ScrollToTop";
 import React from "react";
 
@@ -14,8 +14,15 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/home" element={<Home />} />
+        <Route path="/home/*" element={<Navigate to={"/"} replace />} />
+
         <Route path="/about" element={<About />} />
+        <Route path="/about/*" element={<Navigate to={"/about"} replace />} />
+
         <Route path="/companies" element={<Companies />} />
+        <Route path="/companies/*" element={<Navigate to={"/companies"} replace />} />
+
+        <Route path="*" element={<Navigate to={"/"} replace />} />
       </Routes>
     </BrowserRouter>
   );
