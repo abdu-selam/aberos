73764328a82b677c4a2e3c4f84d0e2004fc6a75f
Abduselam Awel <abdselam676@gmail.com>
2026-08-25 15:18:40 +0300

    restart the project

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,11 @@
-import { BrowserRouter, Routes, Route, Navigate } from "react-router";
-import ScrollToTop from "./components/ScrollToTop";
-import React from "react";
-import chatBot from "./utils/chatbase";
+import React from 'react'
 
-const Home = React.lazy(() => import("./pages/Home"));
-const About = React.lazy(() => import("./pages/About"));
-const Companies = React.lazy(() => import("./pages/Companies"));
-
-function App() {
-  chatBot();
+const App = () => {
   return (
-    <BrowserRouter>
-      <ScrollToTop />
-
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/home" element={<Home />} />
-        <Route path="/home/*" element={<Navigate to={"/"} replace />} />
-
-        <Route path="/about" element={<About />} />
-        <Route path="/about/*" element={<Navigate to={"/about"} replace />} />
-
-        <Route path="/companies" element={<Companies />} />
-        <Route
-          path="/companies/*"
-          element={<Navigate to={"/companies"} replace />}
-        />
-
-        <Route path="*" element={<Navigate to={"/"} replace />} />
-      </Routes>
-    </BrowserRouter>
-  );
+    <div>
+      
+    </div>
+  )
 }
 
-export default App;
+export default App
--- src/store/useStore.js
+++
@@ -1,8 +0,0 @@
-import { create } from "zustand";
-
-const useStore = create((set) => ({
-  lang: "en",
-  setLang: (lang) => set({ lang }),
-}));
-
-export default useStore;
