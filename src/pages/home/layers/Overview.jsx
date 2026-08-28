8b296a90f74fb0f034758e1660f97e066df4ba24
Abduselam Awel <abdselam676@gmail.com>
2026-08-28 16:02:24 +0300

    Fix ui of overview section

--- src/pages/home/layers/Overview.jsx
+++ src/pages/home/layers/Overview.jsx
@@ -1,8 +1,10 @@
+import { FaAngleLeft, FaAngleRight } from "react-icons/fa";
 import image from "../../../assets/hero-bg.webp";
 import MoltenMetal from "../../../components/effects/MoltenMetal";
 
 const Overview = () => {
-  const images = [image, image, image, image];
+  const images = [image, image, image, image, image];
+  const curr = 2;
   const stats = [
     {
       name: "Years of Experience",
@@ -23,52 +25,60 @@ const Overview = () => {
   ];
 
   return (
-    <section className="p-4 pt-16 relative">
-      <div className="absolute top-0 left-0 w-full h-full -z-1">
-        <MoltenMetal color2="#00a98b" color1="#ffffff" color3="#ffffff" opacity={0.5} />
-      </div>
-      <div className="flex flex-col items-center gap-2 py-6">
-        <h2 className="sm:text-[1.8rem] min-[30rem]:text-[1.6rem] text-[1.4rem] font-bold text-center *:text-accent">
+    <section className="pt-8 relative">
+      <div className="flex flex-col items-start gap-2 pt-6 px-4">
+        <h2 className="text-[min(5vw,2.5rem)] font-bold text-center *:text-accent">
           <span className="whitespace-nowrap">Shaping Growth.</span>{" "}
           <span className="whitespace-nowrap">Creating Impact.</span>
         </h2>
-        <p className="text-center max-w-150">
+        <p className="max-w-150">
           We are a dynamic group of companies united by a commitment to
           innovation, quality, and progress.
         </p>
       </div>
-      <ul className="relative min-[60rem]:flex min-[60rem]:items-center min-[60rem]:justify-center min-[60rem]:gap-4 h-107 md:h-97">
+      <ul className="flex overflow-x-hidden scroll-hidden items-center justify-center gap-4 min-[60rem]:mx-auto py-4 relative">
         {images.map((item, i) => (
           <li
             key={i}
-            className={`
-            absolute w-60 aspect-9/13 
-            overflow-hidden rounded-xl left-1/2
-            shadow shadow-text min-[60rem]:static min-[60rem]:translate-x-0 
-            ${
-              i === 0
-                ? "-translate-x-1/2 -translate-y-5 min-[60rem]:-translate-y-15"
-                : i === 1
-                  ? "-translate-x-full md:-translate-x-95 translate-y-20 md:translate-y-10 min-[60rem]:translate-y-0"
-                  : i === 2
-                    ? "translate-x-10 translate-y-10 md:translate-x-35  md:translate-y-10 min-[60rem]:translate-y-0"
-                    : "max-[60rem]:hidden min-[60rem]:-translate-y-15"
-            }
+            className={`${
+              i === 2
+                ? "w-8/10 max-w-110 opacity-100"
+                : Math.abs(2 - i) === 1
+                  ? "w-6/10 max-w-82.5 opacity-60"
+                  : "w-4/10 max-w-55 opacity-40"
+            } shrink-0 aspect-9/13 
+            overflow-hidden flex items-center justify-center
+            relative
             `}
           >
-            <img className="w-full h-full object-cover" src={item} alt="" />
+            <figure className="h-full w-full relative">
+              <img className="w-full h-full object-cover" src={item} alt="" />
+              {curr === i && (
+                <figcaption className="absolute w-full bottom-0 p-4 bg-linear-0 from-back/70 from-60% to-transparent flex flex-col gap-3">
+                  <h3 className="text-lg font-bold">
+                    Lorem ipsum dolor sit amet consectetur.
+                  </h3>
+                  <p className="text-sm opacity-80">
+                    Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+                    Cupiditate officia ipsum unde illo consequatur debitis.
+                  </p>
+                </figcaption>
+              )}
+            </figure>
           </li>
         ))}
       </ul>
-      <p className="text-center max-w-150 mx-auto py-4">
-        Together, we build strong businesses and create opportunities that move
-        industries forward.
-      </p>
-      <h2 className="sm:text-[1.8rem] min-[30rem]:text-[1.6rem] text-[1.4rem] font-bold text-center *:text-accent">
+      <div className="flex justify-end px-4">
+        <p className="max-w-150 w-9/10 pb-4">
+          Together, we build strong businesses and create opportunities that
+          move industries forward.
+        </p>
+      </div>
+      <h2 className="px-4 sm:text-[1.8rem] min-[30rem]:text-[1.6rem] text-[1.4rem] font-bold text-center *:text-accent">
         <span className="whitespace-nowrap">Our Story,</span>{" "}
         <span className="whitespace-nowrap">By the Numbers</span>
       </h2>
-      <ul className="grid grid-cols-2 md:grid-cols-4 gap-4 py-4 mx-auto max-w-5xl">
+      <ul className="grid grid-cols-2 md:grid-cols-4 gap-4 p-4 mx-auto max-w-5xl">
         {stats.map((item, i) => (
           <li
             className="flex flex-col gap-2 items-center border border-text/20 p-4 rounded-2xl bg-back/10 backdrop-blur-md"
@@ -79,7 +89,7 @@ const Overview = () => {
           </li>
         ))}
       </ul>
-      <p className="text-center mx-auto max-w-120">
+      <p className="text-center mx-auto max-w-120 px-4">
         Every number tells a story of our progress, achievements, and commitment
         to creating lasting value.
       </p>
