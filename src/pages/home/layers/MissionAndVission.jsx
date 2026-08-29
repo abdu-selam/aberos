3a02c1c6cb336c41f8c9f55d2b978cfee22c3b0a
Abduselam Awel <abdselam676@gmail.com>
2026-08-29 23:02:16 +0300

    add animation to mission and vission of the home page

--- src/pages/home/layers/MissionAndVission.jsx
+++ src/pages/home/layers/MissionAndVission.jsx
@@ -1,5 +1,7 @@
+import { motion } from "framer-motion";
 import bgOne from "../../../assets/bg-4.webp";
 import bgTwo from "../../../assets/bg-5.webp";
+import AppleTyping from "../../../components/effects/AppleTyping";
 
 const MissionAndVission = () => {
   const data = [
@@ -20,16 +22,48 @@ const MissionAndVission = () => {
         {data.map((item, i) => (
           <li className="h-screen" key={i}>
             <figure className="h-8/10 relative md:grid grid-cols-2 md:px-4 md:gap-4">
-              <img
+              <motion.img
+                initial={{
+                  x: i === 0 ? -30 : 30,
+                  opacity: 0,
+                }}
+                whileInView={{
+                  x: 0,
+                  opacity: 1,
+                }}
+                viewport={{
+                  amount: 0.3,
+                }}
                 className={`h-full md:w-full object-cover ${i === 1 && "md:col-start-2 md:col-end-3 row-span-2"}`}
                 src={item.image}
                 alt=""
               />
               <figcaption
-                className={`max-md:absolute bottom-0 md:w-full bg-linear-0 from-back/70 from-60% to-transparent max-md:px-4 md:self-end md:p-4 md:-translate-y-20 shrink-0 ${i === 1 ? "md:col-start-1 md:col-end-1 md:translate-x-20 text-end": "md:-translate-x-20"}`}
+                className={`max-md:absolute bottom-0 md:w-full bg-linear-0 from-back/70 from-60% to-transparent max-md:px-4 md:self-end md:p-4 md:-translate-y-20 shrink-0 ${i === 1 ? "md:col-start-1 md:col-end-1 md:translate-x-20 text-end" : "md:-translate-x-20"}`}
               >
-                <h2 className="text-3xl font-runalto font-semibold mix-blend-difference">{item.title}</h2>
-                <p className="text-sm">{item.text}</p>
+                <h2>
+                  <AppleTyping
+                    text={item.title}
+                    amount={1}
+                    once={false}
+                    className="text-3xl font-runalto font-semibold mix-blend-difference"
+                  />
+                </h2>
+                <motion.p
+                  initial={{
+                    opacity: 0,
+                  }}
+                  whileInView={{
+                    opacity: 1,
+                  }}
+                  transition={{
+                    delay: 0.1,
+                  }}
+                  viewport={{ amount: 1 }}
+                  className="text-sm"
+                >
+                  {item.text}
+                </motion.p>
               </figcaption>
             </figure>
           </li>
