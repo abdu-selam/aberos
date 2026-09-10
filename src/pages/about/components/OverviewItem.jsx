8a7ae2517b139c59fbfa1c3e55dbd424b7e20f6a
Abduselam Awel <abdselam676@gmail.com>
2026-09-10 17:25:36 +0300

    animate overview section of the about page

--- src/pages/about/components/OverviewItem.jsx
+++ src/pages/about/components/OverviewItem.jsx
@@ -1,26 +1,46 @@
+import { motion } from "framer-motion";
+import AppleTyping from "../../../components/effects/AppleTyping";
+
 const OverviewItem = ({ title, description, image, i }) => {
   return (
     <li className="max-sm:max-w-100 w-full">
       <figure
         className={`flex max-sm:flex-col w-full gap-4 items-center sm:items-end ${i % 2 === 1 && "sm:flex-row-reverse"}`}
       >
-        <div className="w-full aspect-451/318 shadow-[0_0_0.3rem] shadow-back/20 object-cover object-center overflow-hidden">
+        <motion.div
+          initial={{
+            x: i % 2 === 1 ? 25 : -25,
+            opacity: 0,
+          }}
+          whileInView={{
+            x: 0,
+            opacity: 1,
+          }}
+          viewport={{
+            amount: 0.6,
+            once: true,
+          }}
+          transition={{
+            duration: 0.7,
+          }}
+          className="w-full aspect-451/318 shadow-[0_0_0.3rem] shadow-back/20 object-cover object-center overflow-hidden"
+        >
           <img
             className="w-full h-full hover:scale-105 object-cover object-center transition"
             src={image}
             alt={`image of overview ${title} for the aberos about page`}
           />
-        </div>
+        </motion.div>
         <figcaption className="flex w-full max-w-85 flex-col gap-2 sm:pb-10">
           <h4
-            className={`text-back sm:text-xl md:text-2xl font-runalto font-semibold max-sm:text-center ${i % 2 === 1 && "sm:text-end"}`}
+            className={`*:text-back sm:text-xl md:text-2xl *:font-runalto font-semibold max-sm:text-center ${i % 2 === 1 && "sm:text-end"}`}
           >
-            {title}
+            <AppleTyping text={title} />
           </h4>
           <p
-            className={`text-back text-sm max-sm:text-center ${i % 2 === 1 && "sm:text-end"}`}
+            className={`*:text-back text-sm max-sm:text-center ${i % 2 === 1 && "sm:text-end"}`}
           >
-            {description}
+            <AppleTyping text={description} />
           </p>
         </figcaption>
       </figure>
