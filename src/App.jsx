364a05585b71259574338ddaac185ef75b7f47a2
Abduselam Awel <abdselam676@gmail.com>
2026-09-11 16:26:42 +0300

    the scroll position on route change has been handled

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,19 @@
 import React, { useEffect } from "react";
-import { Route, Routes } from "react-router-dom";
+import { Route, Routes, useLocation } from "react-router-dom";
 import Home from "./pages/home/Home";
 import Header from "./components/ui/Header";
 import Footer from "./components/ui/Footer";
 import About from "./pages/about/About";
 
 const App = () => {
+  const location = useLocation();
+
   useEffect(() => {
     window.scrollTo({
       top: 0,
       behavior: "instant",
     });
-  }, []);
+  }, [location.pathname]);
 
   return (
     <>
