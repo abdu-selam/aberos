5cd4106ee2d62201179daf04d6241ea53e2a9d0e
Abduselam Awel <abdselam676@gmail.com>
2026-09-11 17:26:43 +0300

    add style for the hero section of companies page

--- src/pages/companies/layouts/Hero.jsx
+++ src/pages/companies/layouts/Hero.jsx
@@ -1,6 +1,7 @@
 import mall from "../../../assets/mall.webp";
 import restaurant from "../../../assets/restaurant.webp";
 import factory from "../../../assets/factory.webp";
+import { useRef } from "react";
 
 const Hero = () => {
   const links = [
@@ -17,37 +18,52 @@ const Hero = () => {
       link: "#factory",
     },
   ];
+  const linksRef = useRef([]);
 
   return (
-    <section>
-      <h1>Built for Lasting Value</h1>
-      <p>
-        Aberos PLC brings together businesses across retail, hospitality, and
-        manufacturing, creating opportunities that connect people, strengthen
-        communities, and contribute to sustainable economic growth.
-      </p>
-      <ul>
-        {links.map((item, i) => (
-          <li key={i}>
-            <a href={item.link}>{item.name}</a>
-          </li>
-        ))}
-      </ul>
-      <ul className="flex">
-        {[mall, restaurant, factory].map((item, i) => (
-          <li
-            className="w-60 aspect-square overflow-hidden rounded-xl [clip-path:polygon(0_50%,50%_0,100%_50%,50%_100%)]"
-            key={i}
-          >
-            <img
-              className="w-full h-full object-cover"
-              width={100}
-              src={item}
-              alt={`image of aberos subfactory ${i} in the hero section of companies page`}
-            />
-          </li>
-        ))}
-      </ul>
+    <section className="h-[calc(100dvh-2.8rem)] flex flex-col items-center justify-center bg-text relative">
+      <div className="absolute bottom-0 left-1/2 -translate-x-1/2 h-screen -z-1 bg-text w-screen"></div>
+      <div className="flex flex-col items-center justify-center -translate-y-12">
+        <h1 className="text-[min(8vw,6rem)] leading-none px-4 font-runalto font-bold text-accent pb-3">
+          Built for Lasting Value
+        </h1>
+        <p className="text-back text-center text-xs px-4 pb-3 max-w-150">
+          Aberos PLC brings together businesses across retail, hospitality, and
+          manufacturing, creating opportunities that connect people, strengthen
+          communities, and contribute to sustainable economic growth.
+        </p>
+        <ul className="flex gap-3 w-max">
+          {links.map((item, i) => (
+            <li className={`w-max`} key={i}>
+              <a
+                ref={(el) => (linksRef.current[i] = el)}
+                className="text-accent hover:text-back transition font-runalto font-semibold text-sm"
+                href={item.link}
+              >
+                {item.name}
+              </a>
+            </li>
+          ))}
+        </ul>
+        <ul className="flex max-w-120 pt-16">
+          {[mall, restaurant, factory].map((item, i) => (
+            <li
+              className={`w-full aspect-square overflow-hidden rounded-xl rotate-45  ${i === 1 ? "translate-y-1/3" : "-translate-y-1/3"}`}
+              key={i}
+            >
+              <img
+                onClick={() => {
+                  linksRef.current[i].click();
+                }}
+                className={`w-full h-full object-cover -rotate-45 scale-140 hover:scale-150 transition`}
+                width={100}
+                src={item}
+                alt={`image of aberos subfactory ${i} in the hero section of companies page`}
+              />
+            </li>
+          ))}
+        </ul>
+      </div>
       <ul>
         <li></li>
       </ul>
