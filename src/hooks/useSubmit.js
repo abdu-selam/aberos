b669d30a2683c8ed57291695a58ce99859d51194
Abduselam Awel <abdselam676@gmail.com>
2026-09-15 16:41:05 +0300

    add send email fetcher function

--- src/hooks/useSubmit.js
+++ src/hooks/useSubmit.js
@@ -13,15 +13,57 @@ export const useSubmit = (ref) => {
     nextInput(ref, el);
   };
 
-  const submit = (e) => {
+  const submit = async (e) => {
     e.preventDefault();
-    const form = e.currentTarget;
+    const form = ref.current;
 
     const validation = nextInput(ref);
 
     const goon = tooltipHelper(validation, timeout, setTooltip);
     if (!goon) return;
+
+    const data = {
+      name: form.name.value,
+      email: form.email.value,
+      message: form.message.value,
+    };
+
+    const res = await fetcher(data);
   };
 
   return { submit, tooltip, nextInput: nextInputFunc };
+};
+
+const fetcher = async (data) => {
+  const SERVER_ADRESS = import.meta.env.VITE_SERVER_URL;
+
+  try {
+    const res = await fetch(`${SERVER_ADRESS}/email`, {
+      method: "POST",
+      body: JSON.stringify(data),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+
+    const status = res.status;
+    const response = {
+      status: status === 200,
+      message: "Thank you for your message",
+    };
+
+    if (status !== 200) {
+      response.message =
+        status === 400
+          ? "Please fill fields properly"
+          : "Something went wrong! try again later!";
+    }
+
+    return response;
+  } catch (error) {
+    return {
+      status: false,
+      message: "Something went wrong! try again later!",
+    };
+  }
 };
